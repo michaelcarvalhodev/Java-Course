1c04b69bd271b552462aa8e7b28e2cdb3a12c1fe
mmmichael-carvalho <162504810+mmmichael-carvalho@users.noreply.github.com>
2025-11-26 13:10:04 -0300

    upd

--- src/arrays/Matriz.java
+++ src/arrays/Matriz.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Matriz {
 
     public static void main(String[] args) {
+
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("Quantos alunos? ");
---
+++ src/fundamentos/operadores/CalculoDeAreas.java
@@ -0,0 +1,39 @@
+package fundamentos.operadores;
+
+import java.util.Scanner;
+
+public class CalculoDeAreas {
+
+//    **Crie um programa que:**
+//   - Declare constantes para PI (3.14159) e o raio de um círculo
+//   - Calcule a área do círculo (π × r²)
+//   - Calcule o perímetro do círculo (2 × π × r)
+//   - Imprima os resultados formatados com 2 casas decimais
+
+    public static void main(String[] args) {
+
+        final double pi = 3.14159;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Qual o raio do circulo?: ");
+        double r = input.nextDouble();
+
+        System.out.println("Calculando a area do circulo pelo raio");
+
+        double a = (r * r) * pi;
+
+        System.out.printf("Area do circulo = %.2f",a);
+
+
+        System.out.println("\n\nCalculando o perímetro do circulo pelo raio");
+        double p = 2 * pi * r;
+        System.out.printf("Perimetro do circulo = %.2f",p);
+
+
+
+        input.close();
+
+
+    }
+}
