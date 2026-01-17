f8e3c7df652b2f2f3a0065f85128d4887c015388
Michael Carvalho <162504810+michaelcarvalhodev@users.noreply.github.com>
2026-01-17 20:48:31 -0300

    Update

--- src/exercicios/lambda/jogoRPG/Personagem.java
+++ src/exercicios/lambda/jogoRPG/Personagem.java
@@ -59,18 +59,6 @@ public abstract class Personagem {
                 "\nNivel: " + this.getNivel() +
                 "\nExperiencia: " + this.getExperiencia() +
                 "\nDinheiro: " + this.getDinheiro();
-//                this.nome = nome;
-//        this.alcanceDeAtaque = alcanceDeAtaque;
-//        this.vida = vida;
-//        this.forca = forca;
-//        this.nivel = 1;
-//        this.dinheiro = 0;
-//        this.experiencia = 0;
-//        this.x = x;
-//        this.y = y;
-//                "Forca: " + this.getForca() +
-//                "Vida: " + this.getVida();
-//                ": " + this.getVida();
     }
 
     public void comprarItem(Item item) {
